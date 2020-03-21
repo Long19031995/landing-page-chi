bf30ab9217b8249de4491b60e66973653a239c68
chitrankim <chitran@foxcommerce.co>
2020-03-21 21:02:20 +0700

    code

--- src/components/Body/OverchargeCost.vue
+++ src/components/Body/OverchargeCost.vue
@@ -1,5 +1,37 @@
 <template>
     <div class="overcharge-cost">
         <h1>Overcharge cost</h1>
+        <table>
+            <tr>
+                <template v-for="(val, key) in costs">
+                    <th class="py12" :key="key">{{ val.type }}</th>
+                </template>
+            </tr>
+            <tr>
+                <template v-for="(val, key) in costs">
+                    <td class="text-center py24 align-center" :key="key">
+                        {{ val.value }}<img :src="iconCent">
+                    </td>
+                </template>
+            </tr>
+        </table>
     </div>
 </template>
+
+<script>
+import { pricingContent } from '../../core/constant/pricing'
+
+export  default {
+    name: 'overcharge-cost',
+    data () {
+        return {
+            costs: pricingContent.overchargeCost
+        }
+    },
+    computed: {
+        iconCent () {
+            return require('@/assets/image/logo-icon/cent.svg')
+        }
+    }
+}
+</script>
