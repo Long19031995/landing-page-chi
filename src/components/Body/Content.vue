b645fc26bd3a554502a0a2b573794fab5c3ce11a
chitrankim <chitran@foxcommerce.co>
2020-03-21 12:30:00 +0700

    pricing content

--- src/components/Body/Content.vue
+++ src/components/Body/Content.vue
@@ -1,29 +1,33 @@
 <template>
-    <div class="content flex bg-white black-darker">
-        <div class="col-1">
-            Encoding Minutes (mins)
-        </div>
-        <div class="col-2">
-            200
-        </div>
-        <div class="col-3">
-            2.000
-        </div>
-        <div class="col-4">
-            20.000
-        </div>
-        <div class="col-5">
-            <div class="checked flex align-center justify-center">
-                <img :src="checked">
+    <div class="pricing-feature">
+        <template v-for="(val, index) in pageData.defaultFeatures">
+            <div :key="index" class="content flex bg-white black-darker">
+                <div class="col-1 feature-name">{{ val.name }}</div>
+                <div class="col-2 col-developer">{{ val.developer }}</div>
+                <div class="col-3 col-team">{{ val.team}}</div>
+                <div class="col-4 col-business">{{ val.business }}</div>
+                <div class="col-5 col-enterprise">
+                    <div v-if="val.enterprise !== 1" class=""> {{ val.enterprise}}</div>
+                    <div v-else class="checked flex align-center justify-center">
+                        <img :src="checked">
+                    </div>
+                </div>
             </div>
-        </div>
+        </template>
     </div>
 </template>
 
 <script>
+import { pricingContent } from '../../core/constant/pricing'
+
 export default {
     name: 'Content',
+    data() {
+        return {
+            pageData: pricingContent,
 
+        }
+    },
     computed: {
         checked() {
             return require('@/assets/image/check.png')
