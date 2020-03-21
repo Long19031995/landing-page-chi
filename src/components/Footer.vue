841eceef19a2647b7f5921fc81747ae9e3c362bd
chitrankim <chitran@foxcommerce.co>
2020-03-21 11:09:23 +0700

    onboard code

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,83 +1,59 @@
 <template>
     <div class="footer bg-white black-darker flex flex-column align-center">
-        <h1>Commonly Asked Questions</h1>
+        <h1>{{pageData.question}}</h1>
         <div class="flex">
-            <div class="col-md-6">
-                <div class="flex">
-                    <div class="white">
-                        <div class="question-number flex justify-center align-center">
-                            1
+            <template v-for="(q, key) in pageData.contentQuestions.slice(0,2)">
+                <div :key="key" class="col-md-6 col-xl-6">
+                    <div class="flex">
+                        <div class="white">
+                            <div class="question-number flex justify-center align-center">
+                                {{ key + 1}}
+                            </div>
                         </div>
-                    </div>
-                    <div class="question-content">
-                        <div class="question">
-                            How do we calculate Storage?
-                        </div>
-                        <div class="answer">
-                            Peak data stored of all video assets (including live DVR) within the billing period.
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="flex">
-                    <div class="white">
-                        <div class="question-number flex justify-center align-center">
-                            1
-                        </div>
-                    </div>
-                    <div class="question-content">
-                        <div class="question">
-                            How do we calculate Storage?
-                        </div>
-                        <div class="answer">
-                            Peak data stored of all video assets (including live DVR) within the billing period.
+                        <div class="question-content">
+                            <div class="question">
+                                {{ q.q }}
+                            </div>
+                            <div class="answer">
+                                {{ q.ans }}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </template>
         </div>
         <div class="flex">
-            <div class="col-md-6">
-                <div class="flex">
-                    <div class="white">
-                        <div class="question-number flex justify-center align-center">
-                            1
+            <template v-for="(q, key) in pageData.contentQuestions.slice(2,4)">
+                <div :key="key" class="col-md-6 col-xl-6">
+                    <div class="flex">
+                        <div class="white">
+                            <div class="question-number flex justify-center align-center">
+                                {{ key + 3}}
+                            </div>
                         </div>
-                    </div>
-                    <div class="question-content">
-                        <div class="question">
-                            How do we calculate Storage?
-                        </div>
-                        <div class="answer">
-                            Peak data stored of all video assets (including live DVR) within the billing period.
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="flex">
-                    <div class="white">
-                        <div class="question-number flex justify-center align-center">
-                            1
-                        </div>
-                    </div>
-                    <div class="question-content">
-                        <div class="question">
-                            How do we calculate Storage?
-                        </div>
-                        <div class="answer">
-                            Peak data stored of all video assets (including live DVR) within the billing period.
+                        <div class="question-content">
+                            <div class="question">
+                                {{ q.q }}
+                            </div>
+                            <div class="answer">
+                                {{ q.ans }}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </template>
         </div>
     </div>
 </template>
 
 <script>
+import { footerContent } from '../core/constant/footer'
 export default {
-    name: 'Footer'
+    name: 'Footer',
+    data() {
+        return {
+            pageData: footerContent,
+        }
+    },
 }
 </script>
--- src/core/constant/footer.js
+++ src/core/constant/footer.js
@@ -3,22 +3,22 @@ export const footerContent = {
     contentQuestions: [
         {
             q: 'How do we calculate Storage?',
-            des: 'Peak data stored of all video assets (including live DVR) within the billing period.'
+            ans: 'Peak data stored of all video assets (including live DVR) within the billing period.'
         },
         {
             q: 'How do we calculate Delivery?',
-            des: 'Total data delivered within billing period. Reset to 0 at the begining of the new billing period.'
+            ans: 'Total data delivered within billing period. Reset to 0 at the begining of the new billing period.'
         },
         {
             q: 'How do we calculate Encoding Minutes?',
-            des: 'Profile determine encoding minutes. Three attribute affect encoding minutes: Video Resolution, Codec, and Video Framerate.' +
+            ans: 'Profile determine encoding minutes. Three attribute affect encoding minutes: Video Resolution, Codec, and Video Framerate.' +
                 'Each profile adds encoding minutes with the multiplier of the tree attributes. For non-encoding live stream, the duration ' +
                 'of the event will be counted as encoding minutes.' +
                 'Click here for an example.'
         },
         {
             q: 'What happen if I have higher usage than my plan?',
-            des: 'Usage beyond those included in the plan will be changed at the rates listed above.' + '/br' + 'For your protection' +
+            ans: 'Usage beyond those included in the plan will be changed at the rates listed above.' + '/br' + 'For your protection' +
                 'each package also has a monthly maximum. If you exceed this value in a given month, the account will be suspended util you upgrade to a higher plan or contact support.'
         }
     ]
